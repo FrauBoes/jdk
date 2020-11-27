171a1029dc53a40ae52709582262977acf75429c
Julia Boes <julia.boes@oracle.com>
2020-11-27 18:24:05 +0000

    first commit

--- src/jdk.httpserver/share/classes/com/sun/net/httpserver/SimpleFileServer.java
+++ src/jdk.httpserver/share/classes/com/sun/net/httpserver/SimpleFileServer.java
@@ -32,6 +32,7 @@ import java.io.UncheckedIOException;
 import java.net.InetSocketAddress;
 import java.net.URLConnection;
 import java.nio.file.Path;
+import java.util.function.Function;
 
 /**
  * A class that provides a simple HTTP file server and its components for
@@ -57,16 +58,16 @@ import java.nio.file.Path;
  * <p>Example of a simple file server:
  * <pre>    {@code var server = SimpleFileServer.createFileServer(8080, Path.of("/some/path"), OutputLevel.DEFAULT);
  *    server.start();}</pre>
- * <p><b>File handler</b><p>
- * {@link #createFileHandler(Path)} returns a {@code HttpHandler} that
+ * <p><b>File server handler</b><p>
+ * {@link #createFileServerHandler(Path)} returns a {@code HttpHandler} that
  * displays the static content of the given directory in HTML. The handler can
  * serve directory listings and files, the content type of a file is determined
- * on a {@linkplain #createFileHandler(Path) best-guess}. The handler
+ * on a {@linkplain #createFileServerHandler(Path) best-guess}. The handler
  * supports only HEAD and GET requests; to handle request methods other than
  * HEAD and GET, the handler instance can be complemented by the server's other
  * handlers, or it can be wrapped by a custom {@code HttpHandler}.
  * <p>Example complemented file handler:
- * <pre>    {@code var handler = SimpleFileServer.createFileHandler(Path.of("/some/path/"));
+ * <pre>    {@code var handler = SimpleFileServer.createFileServerHandler(Path.of("/some/path/"));
  *    var server = HttpServer.create(new InetSocketAddress(8080), 10, "/browse/", handler);
  *    server.createContext("/echo/", new PostHandler());
  *    server.start();
@@ -82,7 +83,7 @@ import java.nio.file.Path;
  *    server.start();
  *    ...
  *    class WrappedHandler implements HttpHandler {
- *        private static final HttpHandler fileServerHandler = SimpleFileServer.createFileHandler(Path.of("/some/path/"));
+ *        private static final HttpHandler fileServerHandler = SimpleFileServer.createFileServerHandler(Path.of("/some/path/"));
  *
  *        @Override
  *        public void handle(HttpExchange t) throws IOException {
@@ -113,112 +114,117 @@ import java.nio.file.Path;
  */
 public final class SimpleFileServer {
 
-	 private SimpleFileServer () {
-	 }
+    private static final Function<String, String> MIME_TABLE =
+            s -> URLConnection.getFileNameMap().getContentTypeFor(s);
 
-	 /**
-	  * Describes the output produced by a {@code HttpExchange}.
-	  */
-	 public enum OutputLevel {
-		  /**
-			* Used to specify no output.
-			*/
-		  NONE,
-		  /**
-			* Used to specify output in the default format.
-			* <p>
-			* The default format is based on the <a href='https://www.w3.org/Daemon/User/Config/Logging.html#common-logfile-format'>Common Logfile Format</a>.
-			* and includes the following information:
-			* <p>
-			* {@code remotehost rfc931 authuser [date] "request" status bytes}
-			* <p>
-			* Example:
-			* <p>
-			* {@code 127.0.0.1 - - [22/Jun/2000:13:55:36 -0700] "GET /example.txt HTTP/1.0" 200 -}
-			* <p>
-			* Note: The fields {@code rfc931}, {@code authuser} and {@code bytes}
-			* are not captured in the implementation and are always represented as
-			* {@code '-'}.
-			*/
-		  DEFAULT,
-		  /**
-			* Used to specify output in the verbose format.
-			* <p>
-			* Additional to the information provided by the
-			* {@linkplain OutputLevel#DEFAULT default} format, the verbose format
-			* includes the request and response headers of the {@code HttpExchange}.
-			*/
-		  VERBOSE
-	 }
+    private SimpleFileServer() {
+    }
 
-	 /**
-	  * Creates a {@code HttpServer} with a {@code HttpHandler} that displays
-	  * the static content of the given directory in HTML.
-	  * <p>
-	  * The server is bound to the wildcard address and the given port. The handler
-	  * is mapped to the URI path "/" via a {@code HttpContext}. It only supports
-	  * HEAD and GET requests and serves directory listings, html and text files.
-	  * Other MIME types are supported on a best-guess basis.
-	  * <p>
-	  * An optional {@code Filter} that prints information about the
-	  * {@code HttpExchange} to {@code System.out} can be specified via the
-	  * {@linkplain OutputLevel outputLevel} argument. If
-	  * {@link OutputLevel#NONE OutputLevel.NONE} is passed, no {@code Filter} is
-	  * added. Otherwise a {@code Filter} is added with either
-	  * {@linkplain OutputLevel#DEFAULT default} or
-	  * {@linkplain OutputLevel#VERBOSE verbose} output format.
-	  *
-	  * @param port        the port number
-	  * @param root        the root directory to be served, must be an absolute path
-	  * @param outputLevel the output about a http exchange
-	  * @return a HttpServer
-	  * @throws UncheckedIOException
-	  */
-	 public static HttpServer createFileServer (int port, Path root, OutputLevel outputLevel) {
-		  try {
-				return outputLevel.equals(OutputLevel.NONE)
-					? HttpServer.create(new InetSocketAddress(port), 0, "/",
-					new FileServerHandler(root))
-					: HttpServer.create(new InetSocketAddress(port), 0, "/",
-					new FileServerHandler(root), new OutputFilter(System.out, outputLevel));
-		  } catch (IOException ioe) {
-				throw new UncheckedIOException(ioe);
-		  }
-	 }
+    /**
+     * Describes the output produced by a {@code HttpExchange}.
+     */
+    public enum OutputLevel {
+        /**
+         * Used to specify no output.
+         */
+        NONE,
+        /**
+         * Used to specify output in the default format.
+         * <p>
+         * The default format is based on the <a href='https://www.w3.org/Daemon/User/Config/Logging.html#common-logfile-format'>Common Logfile Format</a>.
+         * and includes the following information:
+         * <p>
+         * {@code remotehost rfc931 authuser [date] "request" status bytes}
+         * <p>
+         * Example:
+         * <p>
+         * {@code 127.0.0.1 - - [22/Jun/2000:13:55:36 -0700] "GET /example.txt HTTP/1.0" 200 -}
+         * <p>
+         * Note: The fields {@code rfc931}, {@code authuser} and {@code bytes}
+         * are not captured in the implementation and are always represented as
+         * {@code '-'}.
+         */
+        DEFAULT,
+        /**
+         * Used to specify output in the verbose format.
+         * <p>
+         * Additional to the information provided by the
+         * {@linkplain OutputLevel#DEFAULT default} format, the verbose format
+         * includes the request and response headers of the {@code HttpExchange}.
+         */
+        VERBOSE
+    }
 
-	 /**
-	  * Creates a {@code HttpHandler} that displays the static content of the
-	  * given directory in HTML.
-	  * <p>
-	  * The handler supports only HEAD and GET requests and can serve directory
-	  * listings and files. Content types are supported on a best-guess basis.
-	  *
-	  * @implNote The content type of a file is guessed by calling
-	  *           {@link java.net.FileNameMap#getContentTypeFor(String)} on the
-	  *           {@link URLConnection#getFileNameMap() mimetable} found.
-	  *
-	  * @param root the root directory to be served, must be an absolute path
-	  * @return a HttpHandler
-	  */
-	 public static HttpHandler createFileHandler (Path root) {
-		  return new FileServerHandler(root);
-	 }
+    /**
+     * Creates a {@code HttpServer} with a {@code HttpHandler} that displays
+     * the static content of the given directory in HTML.
+     * <p>
+     * The server is bound to the wildcard address and the given port. The handler
+     * is mapped to the URI path "/" via a {@code HttpContext}. It only supports
+     * HEAD and GET requests and serves directory listings, html and text files.
+     * Other MIME types are supported on a best-guess basis.
+     * <p>
+     * An optional {@code Filter} that prints information about the
+     * {@code HttpExchange} to {@code System.out} can be specified via the
+     * {@linkplain OutputLevel outputLevel} argument. If
+     * {@link OutputLevel#NONE OutputLevel.NONE} is passed, no {@code Filter} is
+     * added. Otherwise a {@code Filter} is added with either
+     * {@linkplain OutputLevel#DEFAULT default} or
+     * {@linkplain OutputLevel#VERBOSE verbose} output format.
+     *
+     * @param port        the port number
+     * @param root        the root directory to be served, must be an absolute path
+     * @param outputLevel the output about a http exchange
+     * @return a HttpServer
+     * @throws UncheckedIOException
+     */
+    public static HttpServer createFileServer(int port,
+                                              Path root,
+                                              OutputLevel outputLevel) {
+        try {
+            return outputLevel.equals(OutputLevel.NONE)
+                    ? HttpServer.create(new InetSocketAddress(port), 0, "/",
+                    FileServerHandler.create(root, MIME_TABLE))
+                    : HttpServer.create(new InetSocketAddress(port), 0, "/",
+                    FileServerHandler.create(root, MIME_TABLE),
+                    new OutputFilter(System.out, outputLevel));
+        } catch (IOException ioe) {
+            throw new UncheckedIOException(ioe);
+        }
+    }
 
-	 /**
-	  * Creates a {@code Filter} that prints output about a {@code HttpExchange}
-	  * to the given {@code OutputStream}.
-	  * <p>
-	  * The output format is specified by the {@link OutputLevel outputLevel}.
-	  *
-	  * @implNote An {@link IllegalArgumentException} is thrown if
-	  * 			  {@link OutputLevel#NONE OutputLevel.NONE} is passed. It is
-	  * 			  recommended to not use a filter in this case.
-	  *
-	  * @param out         the OutputStream to print to
-	  * @param outputLevel the output about a http exchange
-	  * @return a Filter
-	  */
-	 public static Filter createOutputFilter (OutputStream out, OutputLevel outputLevel) {
-		  return new OutputFilter(out, outputLevel);
-	 }
+    /**
+     * Creates a {@code HttpHandler} that displays the static content of the
+     * given directory in HTML.
+     * <p>
+     * The handler supports only HEAD and GET requests and can serve directory
+     * listings and files. Content types are supported on a best-guess basis.
+     *
+     * @param root the root directory to be served, must be an absolute path
+     * @return a HttpHandler
+     * @implNote The content type of a file is guessed by calling
+     * {@link java.net.FileNameMap#getContentTypeFor(String)} on the
+     * {@link URLConnection#getFileNameMap() mimeTable} found.
+     */
+    public static HttpHandler createFileServerHandler(Path root) {
+        return FileServerHandler.create(root, MIME_TABLE);
+    }
+
+    /**
+     * Creates a {@code Filter} that prints output about a {@code HttpExchange}
+     * to the given {@code OutputStream}.
+     * <p>
+     * The output format is specified by the {@link OutputLevel outputLevel}.
+     *
+     * @param out         the OutputStream to print to
+     * @param outputLevel the output about a http exchange
+     * @return a Filter
+     * @implNote An {@link IllegalArgumentException} is thrown if
+     * {@link OutputLevel#NONE OutputLevel.NONE} is passed. It is
+     * recommended to not use a filter in this case.
+     */
+    public static Filter createOutputFilter(OutputStream out,
+                                            OutputLevel outputLevel) {
+        return new OutputFilter(out, outputLevel);
+    }
 }
--- src/jdk.httpserver/share/classes/sun/net/httpserver/FileServerHandler.java
+++ src/jdk.httpserver/share/classes/sun/net/httpserver/FileServerHandler.java
@@ -23,7 +23,6 @@
 
 package sun.net.httpserver;
 
-import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
@@ -32,9 +31,14 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.URI;
-import java.net.URLConnection;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * A basic HTTP file server handler for static content.
@@ -44,152 +48,174 @@ import java.nio.file.Path;
  * served, content types are supported on a best-guess basis.
  */
 public final class FileServerHandler implements HttpHandler {
-    private final Path ROOT;
+    private final Path root;
+    private final Function<String, String> mimeTable;
+    private static final List<String> SUPPORTED_METHODS = List.of("HEAD", "GET");
 
-    public FileServerHandler(Path root) {
-        if (!Files.exists(root)) {
+    private FileServerHandler(Path root, Function<String, String> mimeTable) {
+        if (!Files.exists(root))
             throw new IllegalArgumentException("Path does not exist: " + root);
-        }
-        if (!Files.isDirectory(root)) {
+        if (!Files.isDirectory(root))
             throw new IllegalArgumentException("Path not a directory: " + root);
-        }
-        ROOT = root;
+        this.root = root;
+        this.mimeTable = mimeTable;
     }
 
-    public void handle(HttpExchange t) throws IOException {
-        try {
-            try (InputStream is = t.getRequestBody()) {
-                is.readAllBytes();
-            }
+    public static HttpHandler create(Path root, Function<String, String> mimeTable) {
+        HttpHandler handler = new FileServerHandler(root, mimeTable);
+        return handler.discardingRequestBody()
+                .delegating(FileServerHandler::handleNotAllowed,
+                        Predicate.not(SUPPORTED_METHODS::contains));
+    }
 
-            Headers respHeaders = t.getResponseHeaders();
-            URI uri = t.getRequestURI();
-            String contextPath = t.getHttpContext().getPath();
-            if (! contextPath.endsWith("/")) {
-                contextPath += "/";
-            }
-            String file = URI.create(contextPath).relativize(uri).getPath();
-            if (file.isEmpty())
-                file = "./";
-            Path path = ROOT.resolve(file);
-            if (!Files.exists(path)) {
-                notFound(t, file);
+    static void handleNotAllowed(HttpExchange exchange) throws IOException {
+        try (exchange) {
+            exchange.sendResponseHeaders(405, -1);
+        }
+    }
+
+    void handleHEAD(HttpExchange exchange, Path path) throws IOException {
+        exchange.getResponseHeaders().set("Content-Length", Long.toString(Files.size(path)));
+        exchange.sendResponseHeaders(200, -1);
+    }
+
+    void handleGET(HttpExchange exchange, Path path) throws IOException {
+        if (Files.isDirectory(path)) {
+            if (testMissingSlash(exchange)) {
+                handleMovedPermanently(exchange);
                 return;
             }
-
-            String method = t.getRequestMethod();
-            if (method.equals("HEAD")) {
-                respHeaders.set("Content-Length", Long.toString(Files.size(path)));
-                t.sendResponseHeaders(200, -1);
-                t.close();
-            } else if (!method.equals("GET")) {
-                t.sendResponseHeaders(405, -1);
-                t.close();
-                return;
-            }
-
-            // best-guess MIME type
-            String type = getMediaType(file);
-            respHeaders.set("Content-Type", type);
-
-            if (Files.isDirectory(path)) {
-                if (!file.endsWith("/")) {
-                    redirect(t);
-                    return;
-                }
-                respHeaders.set("Content-Type", "text/html");
-                Path index = getIndex(path);
-                if (index != null) {
-                    serveFile(t, index);
-                } else {
-                    listFiles(t, path, file);
-                }
+            if (indexFile(path) != null) {
+                serveFile(exchange, indexFile(path));
             } else {
-                serveFile(t, path);
+                listFiles(exchange, path);
             }
-        } catch (Throwable throwable) {
-            throwable.printStackTrace();
-            throw throwable;
+        } else {
+            serveFile(exchange, path);
         }
     }
 
-    private Path getIndex(Path path) {
-        if (Files.exists(path.resolve("index.html"))) {
-            return path.resolve("index.html");
+    void handleNotFound(HttpExchange exchange) throws IOException {
+        exchange.getResponseHeaders().set("Content-Type", "text/html");
+        exchange.sendResponseHeaders(404, 0);
+        try (OutputStream os = exchange.getResponseBody()) {
+            os.write(("<h2>File not found</h2>"
+                    + sanitize.apply(exchange.getRequestURI().getPath(), chars)
+                    + "<p>").getBytes());
         }
-        if (Files.exists(path.resolve("index.htm"))) {
-            return path.resolve("index.htm");
-        }
-        return null;
     }
 
-    private void serveFile(HttpExchange t, Path path) throws IOException {
-        t.sendResponseHeaders(200, 0);
+    void handleMovedPermanently(HttpExchange exchange) throws IOException {
+        exchange.getResponseHeaders().set("Content-Type", "text/html");
+        exchange.getResponseHeaders().set("Location", "http://"
+                + exchange.getRequestHeaders().getFirst("Host")
+                + exchange.getRequestURI().getPath() + "/");
+        exchange.sendResponseHeaders(301, -1);
+    }
+
+    boolean testNotFound(HttpExchange exchange, Path path) throws IOException {
+        if (!Files.exists(path)) {
+            handleNotFound(exchange);
+            return true;
+        }
+        return false;
+    }
+
+    boolean testMissingSlash(HttpExchange exchange) throws IOException {
+        if (!exchange.getRequestURI().getPath().endsWith("/")) {
+            handleMovedPermanently(exchange);
+            return true;
+        }
+        return false;
+    }
+
+    Path mapToPath(HttpExchange exchange, Path root) {
+        URI uri = exchange.getRequestURI();
+        String contextPath = exchange.getHttpContext().getPath();
+        if (!contextPath.endsWith("/")) contextPath += "/";
+        String file = URI.create(contextPath).relativize(uri).getPath();
+        if (file.isEmpty()) file = "./";
+        return root.resolve(file);
+    }
+
+    Path indexFile(Path path) {
+        Path html = path.resolve("indexFile.html");
+        Path htm = path.resolve("indexFile.htm");
+        return Files.exists(html) ? html : Files.exists(htm) ? htm : null;
+    }
+
+    void serveFile(HttpExchange exchange, Path path) throws IOException {
+        exchange.sendResponseHeaders(200, 0);
+        exchange.getResponseHeaders().set("Content-Type",
+                mediaType(exchange.getRequestURI().getPath()));
         try (InputStream fis = Files.newInputStream(path);
-             OutputStream os = t.getResponseBody()) {
+             OutputStream os = exchange.getResponseBody()) {
             fis.transferTo(os);
         }
     }
 
-    private String getMediaType(String file) {
-        String type = URLConnection.getFileNameMap().getContentTypeFor(file);
-        return type != null ? type : "content/unknown";
-        // the latter causes a save dialog to be presented to the user
-    }
-
-    private void listFiles(HttpExchange t, Path path, String file)
-            throws IOException {
-        t.sendResponseHeaders(200, 0);
-        try (OutputStream os = t.getResponseBody();
+    void listFiles(HttpExchange exchange, Path path) throws IOException {
+        exchange.sendResponseHeaders(200, 0);
+        exchange.getResponseHeaders().set("Content-Type", "text/html");
+        try (OutputStream os = exchange.getResponseBody();
              PrintStream ps = new PrintStream(os)) {
-            ps.println("<!DOCTYPE html>");
-            ps.println("<html>");
-            ps.println("<body>");
-            ps.println("<h2>Directory listing for " + sanitize(file) + "</h2>");
-            ps.println("<ul>");
-            Files.list(path).map(p -> path.toUri().relativize(p.toUri()).toASCIIString())
-                    .forEach(uri ->
-                            ps.println("<li><a href=\"" + uri + "\">" + sanitize(uri) + "</a></li>"));
-            ps.println("</ul><p><hr>");
-            ps.println("</body>");
-            ps.println("</html>");
+            ps.println(openHTML
+                    + "<h2>Directory listing for "
+                    + sanitize.apply(exchange.getRequestURI().getPath(), chars)
+                    + "</h2>\n" + "<ul>");
+            printDirContent(ps, path);
+            ps.println(closeHTML);
             ps.flush();
         }
     }
 
-    private static String sanitize(String file) {
-        var sb = new StringBuilder();
-        file.chars().forEach(c -> sb.append(switch (c) {
-            case (int) '&' -> "&amp;";
-            case (int) '<' -> "&lt;";
-            case (int) '>' -> "&gt;";
-            case (int) '"' -> "&quot;";
-            case (int) '\'' -> "&#x27;";
-            case (int) '/' -> "&#x2F;";
-            default -> Character.toString(c);
-        }));
-        return sb.toString();
+    String openHTML = """
+                <!DOCTYPE html>
+                <html>
+                <body>""";
+
+    String closeHTML = """
+                </ul><p><hr>
+                </body>
+                </html>""";
+
+    void printDirContent(PrintStream ps, Path path) throws IOException {
+        Files.list(path)
+                .map(p -> path.toUri().relativize(p.toUri()).toASCIIString())
+                .forEach(uri -> ps.println("<li><a href=\"" + uri
+                        + "\">" + sanitize.apply(uri, chars) + "</a></li>"));
     }
 
-    private void redirect(HttpExchange t) throws IOException {
-        Headers reqHeaders = t.getRequestHeaders();
-        Headers respHeaders = t.getResponseHeaders();
-        URI uri = t.getRequestURI();
-        String host = reqHeaders.getFirst("Host");
-        var location = "http://" + host + uri.getPath() + "/";
-        respHeaders.set("Content-Type", "text/html");
-        respHeaders.set("Location", location);
-        try (t) {
-            t.sendResponseHeaders(301, -1);
-        }
+    String mediaType(String file) {
+        String type = mimeTable.apply(file);
+        return type != null ? type : "content/unknown";
     }
 
-    private void notFound(HttpExchange t, String file) throws IOException {
-        t.getResponseHeaders().set("Content-Type", "text/html");
-        t.sendResponseHeaders(404, 0);
-        try (t; OutputStream os = t.getResponseBody()) {
-            var s = "<h2>File not found</h2>" + sanitize(file) + "<p>";
-            os.write(s.getBytes());
+    static final BiFunction<String, HashMap<Integer, String>, String> sanitize =
+            (file, chars) -> file.chars().collect(StringBuilder::new,
+                    (sb, c) -> sb.append(chars.getOrDefault(c, Character.toString(c))),
+                    StringBuilder::append).toString();
+
+    static final HashMap<Integer,String> chars = new HashMap<>(Map.of(
+            (int) '&'  , "&amp;"   ,
+            (int) '<'  , "&lt;"    ,
+            (int) '>'  , "&gt;"    ,
+            (int) '"'  , "&quot;"  ,
+            (int) '\'' , "&#x27;"  ,
+            (int) '/'  , "&#x2F;"  )
+    );
+
+    public void handle(HttpExchange exchange) throws IOException {
+        try (exchange) {
+            Path path = mapToPath(exchange, root);
+            if (testNotFound(exchange, path)) {
+                return;
+            }
+            if (exchange.getRequestMethod().equals("HEAD")) {
+                handleHEAD(exchange, path);
+            } else {
+                handleGET(exchange, path);
+            }
         }
     }
 }
